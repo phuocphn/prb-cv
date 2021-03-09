bea0368f6e7e4e4133d7907df4e92e784b8277d9
phuocphn <phuocphn@gmail.com>
2021-03-09 14:51:54 +0900

    add test_tube, add more hparams

--- cifar10.py
+++ cifar10.py
@@ -10,7 +10,7 @@ from torchvision.datasets import MNIST
 from torchvision import transforms
 import pytorch_lightning as pl
 from pytorch_lightning.metrics.functional import accuracy
-
+from pytorch_lightning.loggers import TestTubeLogger
 
  
 '''ResNet in PyTorch.
@@ -142,22 +142,29 @@ def test():
     y = net(torch.randn(1, 3, 32, 32))
     print(y.size())
 
-# test()
 
 
 class LiMCIFAR10(pl.LightningModule):
     
-    def __init__(self, data_dir='./', learning_rate=0.1):
+    def __init__(self, data_dir='~/data', 
+                    learning_rate=0.1, 
+                    weight_decay=5e-4, 
+                    num_workers=4, batch_size=128):
 
         super().__init__()
 
         # Set our init args as class attributes
-        self.data_dir = data_dir
-        # self.hidden_size = hidden_size
+        self.data_dir = os.path.expanduser(data_dir)
+
         self.learning_rate = learning_rate
+        self.weight_decay = weight_decay
+        self.momentum = momentum
+        self.num_workers = num_workers
+        self.batch_size = batch_size
 
         # Hardcode some dataset specific attributes
         self.num_classes = 10
+
         self.transform_train = transforms.Compose([
             transforms.RandomCrop(32, padding=4),
             transforms.RandomHorizontalFlip(),
@@ -170,7 +177,6 @@ class LiMCIFAR10(pl.LightningModule):
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
         ])
 
-        # Define PyTorch model
         self.model = ResNet18()
         self.criterion = nn.CrossEntropyLoss()
 
@@ -181,6 +187,9 @@ class LiMCIFAR10(pl.LightningModule):
         x, y = batch
         outputs = self.forward(x)
         loss = self.criterion(outputs, y) 
+
+        # Tensorboard logs 
+        self.log('train_loss', loss, prog_bar=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -200,9 +209,8 @@ class LiMCIFAR10(pl.LightningModule):
         return self.validation_step(batch, batch_idx)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.SGD(self.parameters(), lr=self.learning_rate, momentum=0.9, weight_decay=5e-4)
+        optimizer = torch.optim.SGD(self.parameters(), lr=self.learning_rate, momentum=self.momentum, weight_decay=self.weight_decay)
         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
-        # return optimizer
         return [optimizer], [scheduler]
 
 
@@ -214,33 +222,41 @@ class LiMCIFAR10(pl.LightningModule):
     def setup(self, stage=None):
         # if stage == 'fit' or stage is None:
         self.trainset = torchvision.datasets.CIFAR10(
-                root='./data', 
+                root=self.data_dir, 
                 train=True, 
                 download=True, 
                 transform=self.transform_train)
 
         # if stage == 'test' or stage is None:
         self.valset = torchvision.datasets.CIFAR10(
-                root='./data', 
+                root=self.data_dir, 
                 train=False, 
                 download=True, 
                 transform=self.transform_test)
 
     def train_dataloader(self):
-        return DataLoader(self.trainset, batch_size=128, shuffle=True, num_workers=4)
+        return DataLoader(self.trainset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
 
     def val_dataloader(self):
-        return DataLoader(self.valset, batch_size=128, shuffle=False, num_workers=4)
-
-    # def test_dataloader(self):
-    #     return DataLoader(self.mnist_test, batch_size=32, num_workers=4)
+        return DataLoader(self.valset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
 
 
 
 
 def main(hparams):
-    model = LiMCIFAR10()
-    trainer = pl.Trainer(gpus=hparams.gpus, max_epochs=hparams.epochs, progress_bar_refresh_rate=20, distributed_backend='ddp', weights_summary='full')
+    model = LiMCIFAR10(learning_rate=hparams.lr, 
+        weight_decay=hparams.wd, 
+        momentum=hparams.momentum, 
+        batch_size=hparams.batch_size, )
+    logger = TestTubeLogger("tb_logs", name="cifar_10")
+    trainer = pl.Trainer(gpus=hparams.gpus, 
+            logger=logger,
+            max_epochs=hparams.epochs, 
+            deterministic=hparams.deterministic, 
+            add_log_row_interval=10,
+            progress_bar_refresh_rate=20, 
+            distributed_backend='ddp', 
+            weights_summary='full')
     trainer.fit(model)
     trainer.test()
 
@@ -250,6 +266,21 @@ if __name__ == '__main__':
     parser.add_argument('--gpus', default=4, type=int, help='number of GPUs to train on' )
     parser.add_argument('--epochs', default=90, type=int, metavar='N',
                     help='number of total epochs to run')
+    parser.add_argument('--deterministic', dest='deterministic', action='store_true',
+                    help='enable deterministic training')
+
+    parser.add_argument('-b', '--batch-size', default=256, type=int,
+                        metavar='N',
+                        help='mini-batch size (default: 256), this is the total '
+                             'batch size of all GPUs on the current node when '
+                             'using Data Parallel or Distributed Data Parallel')
+    parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+                        metavar='LR', help='initial learning rate', dest='lr')
+    parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                        help='momentum')
+    parser.add_argument('--wd', '--weight-decay', default=5e-4, type=float,
+                        metavar='W', help='weight decay (default: 1e-4)',
+                        dest='weight_decay')
     args = parser.parse_args()
 
     main(args)
