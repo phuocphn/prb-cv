24dc749fb99474ba9bb8316b21880d10e1be0e70
phuocphn <phuocphn@gmail.com>
2021-03-11 23:35:29 +0900

    add cifar-100 models

--- main.py
+++ main.py
@@ -13,7 +13,113 @@ from pytorch_lightning.metrics.functional import accuracy
 from pytorch_lightning.loggers import TestTubeLogger
 from pytorch_lightning.callbacks import ModelCheckpoint
 
-class LiMCIFAR10(pl.LightningModule):
+# class LiMCIFAR10(pl.LightningModule):
+    
+#     def __init__(self, data_dir='~/data',
+#                     arch='ResNet18', 
+#                     learning_rate=0.1, 
+#                     momentum=0.9,
+#                     weight_decay=5e-4, 
+#                     num_workers=2, batch_size=128):
+
+#         super().__init__()
+
+#         # Set our init args as class attributes
+#         self.data_dir = os.path.expanduser(data_dir)
+
+#         self.learning_rate = learning_rate
+#         self.weight_decay = weight_decay
+#         self.momentum = momentum
+#         self.num_workers = num_workers
+#         self.batch_size = batch_size
+
+#         # Hardcode some dataset specific attributes
+#         self.num_classes = 10
+
+#         self.transform_train = transforms.Compose([
+#             transforms.RandomCrop(32, padding=4),
+#             transforms.RandomHorizontalFlip(),
+#             transforms.ToTensor(),
+#             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+#         ])
+
+#         self.transform_test = transforms.Compose([
+#             transforms.ToTensor(),
+#             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+#         ])
+
+#         module = importlib.import_module("models.cifar10")
+#         self.model = getattr(module, arch)()
+#         self.criterion = nn.CrossEntropyLoss()
+
+#     def forward(self, x):
+#         return self.model(x)
+
+#     def on_epoch_start(self):
+#         self.logger.experiment.add_scalar('lr', self.trainer.optimizers[0].param_groups[0]['lr'])
+
+#     def training_step(self, batch, batch_idx):
+#         x, y = batch
+#         outputs = self.forward(x)
+#         loss = self.criterion(outputs, y) 
+
+#         # Tensorboard logs 
+#         self.log('train_loss', loss, prog_bar=False)
+#         return loss
+
+#     def validation_step(self, batch, batch_idx):
+#         x, y = batch
+#         outputs = self.forward(x)
+#         loss = self.criterion(outputs, y) 
+#         preds = torch.argmax(outputs, dim=1)
+#         acc = accuracy(preds, y)
+
+#         # Calling self.log will surface up scalars for you in TensorBoard
+#         self.log('val_loss', loss, prog_bar=True)
+#         self.log('val_acc', acc, prog_bar=True)
+#         return loss
+
+#     def test_step(self, batch, batch_idx):
+#         # Here we just reuse the validation_step for testing
+#         return self.validation_step(batch, batch_idx)
+
+#     def configure_optimizers(self):
+#         optimizer = torch.optim.SGD(self.parameters(), lr=self.learning_rate, momentum=self.momentum, weight_decay=self.weight_decay)
+#         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+#         return [optimizer], [scheduler]
+
+
+#     def prepare_data(self):
+#         torchvision.datasets.CIFAR10(root=self.data_dir, train=True, download=True, transform=self.transform_train)
+#         torchvision.datasets.CIFAR10(root=self.data_dir, train=False, download=True, transform=self.transform_test)
+
+
+#     def setup(self, stage=None):
+#         # if stage == 'fit' or stage is None:
+#         self.trainset = torchvision.datasets.CIFAR10(
+#                 root=self.data_dir, 
+#                 train=True, 
+#                 download=True, 
+#                 transform=self.transform_train)
+
+#         # if stage == 'test' or stage is None:
+#         self.valset = torchvision.datasets.CIFAR10(
+#                 root=self.data_dir, 
+#                 train=False, 
+#                 download=True, 
+#                 transform=self.transform_test)
+
+#     def train_dataloader(self):
+#         return DataLoader(self.trainset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
+
+#     def val_dataloader(self):
+#         return DataLoader(self.valset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
+
+
+
+
+
+class LiMCIFAR100(pl.LightningModule):
     
     def __init__(self, data_dir='~/data',
                     arch='ResNet18', 
@@ -37,18 +143,21 @@ class LiMCIFAR10(pl.LightningModule):
         self.num_classes = 10
 
         self.transform_train = transforms.Compose([
+            #transforms.ToPILImage(),
             transforms.RandomCrop(32, padding=4),
             transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(15),
             transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            transforms.Normalize(mean=(0.5070751592371323, 0.48654887331495095, 0.4409178433670343), 
+                std=(0.2673342858792401, 0.2564384629170883, 0.27615047132568404))
         ])
-
         self.transform_test = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            transforms.Normalize(mean=(0.5070751592371323, 0.48654887331495095, 0.4409178433670343), 
+                std=(0.2673342858792401, 0.2564384629170883, 0.27615047132568404))
         ])
 
-        module = importlib.import_module("models.cifar10")
+        module = importlib.import_module("models.cifar100")
         self.model = getattr(module, arch)()
         self.criterion = nn.CrossEntropyLoss()
 
@@ -90,24 +199,23 @@ class LiMCIFAR10(pl.LightningModule):
 
 
     def prepare_data(self):
-        torchvision.datasets.CIFAR10(root=self.data_dir, train=True, download=True, transform=self.transform_train)
-        torchvision.datasets.CIFAR10(root=self.data_dir, train=False, download=True, transform=self.transform_test)
+        torchvision.datasets.CIFAR100(root=self.data_dir, train=True, download=True, transform=self.transform_train)
+        torchvision.datasets.CIFAR100(root=self.data_dir, train=False, download=True, transform=self.transform_test)
 
 
     def setup(self, stage=None):
-        # if stage == 'fit' or stage is None:
-        self.trainset = torchvision.datasets.CIFAR10(
-                root=self.data_dir, 
-                train=True, 
-                download=True, 
-                transform=self.transform_train)
+        self.trainset = torchvision.datasets.CIFAR100(
+            root=self.data_dir, 
+            train=True, 
+            download=True, 
+            transform=self.transform_train)
 
-        # if stage == 'test' or stage is None:
-        self.valset = torchvision.datasets.CIFAR10(
-                root=self.data_dir, 
-                train=False, 
-                download=True, 
-                transform=self.transform_test)
+        self.valset = torchvision.datasets.CIFAR100(
+            root=self.data_dir, 
+            train=False, 
+            download=True, 
+            transform=self.transform_test)
+
 
     def train_dataloader(self):
         return DataLoader(self.trainset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
@@ -117,83 +225,84 @@ class LiMCIFAR10(pl.LightningModule):
 
 
 
-class LiMMNIST(pl.LightningModule):
+
+# class LiMMNIST(pl.LightningModule):
     
-    def __init__(self, data_dir='~/data',
-                    arch='arch1', 
-                    learning_rate=1.0, 
-                    gamma=0.7, 
-                    num_workers=2, batch_size=64):
+#     def __init__(self, data_dir='~/data',
+#                     arch='arch1', 
+#                     learning_rate=1.0, 
+#                     gamma=0.7, 
+#                     num_workers=2, batch_size=64):
 
-        super().__init__()
+#         super().__init__()
 
-        # Set our init args as class attributes
-        self.data_dir = os.path.expanduser(data_dir)
+#         # Set our init args as class attributes
+#         self.data_dir = os.path.expanduser(data_dir)
 
-        self.learning_rate = learning_rate
-        self.gamma = gamma
-        self.num_workers = num_workers
-        self.batch_size = batch_size
+#         self.learning_rate = learning_rate
+#         self.gamma = gamma
+#         self.num_workers = num_workers
+#         self.batch_size = batch_size
 
-        # Hardcode some dataset specific attributes
-        self.num_classes = 10
-        self.transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])
+#         # Hardcode some dataset specific attributes
+#         self.num_classes = 10
+#         self.transform=transforms.Compose([
+#             transforms.ToTensor(),
+#             transforms.Normalize((0.1307,), (0.3081,))
+#         ])
 
-        module = importlib.import_module("models.mnist")
-        self.model = getattr(module, arch)()
-        self.criterion = nn.NLLLoss()
+#         module = importlib.import_module("models.mnist")
+#         self.model = getattr(module, arch)()
+#         self.criterion = nn.NLLLoss()
 
-    def forward(self, x):
-        return self.model(x)
+#     def forward(self, x):
+#         return self.model(x)
 
-    def on_epoch_start(self):
-        self.logger.experiment.add_scalar('lr', self.trainer.optimizers[0].param_groups[0]['lr'])
+#     def on_epoch_start(self):
+#         self.logger.experiment.add_scalar('lr', self.trainer.optimizers[0].param_groups[0]['lr'])
 
-    def training_step(self, batch, batch_idx):
-        x, y = batch
-        outputs = self.forward(x)
-        loss = self.criterion(outputs, y) 
+#     def training_step(self, batch, batch_idx):
+#         x, y = batch
+#         outputs = self.forward(x)
+#         loss = self.criterion(outputs, y) 
 
-        self.log('train_loss', loss, prog_bar=False)
-        return loss
+#         self.log('train_loss', loss, prog_bar=False)
+#         return loss
 
-    def validation_step(self, batch, batch_idx):
-        x, y = batch
-        outputs = self.forward(x)
-        loss = self.criterion(outputs, y) 
-        preds = torch.argmax(outputs, dim=1)
-        acc = accuracy(preds, y)
+#     def validation_step(self, batch, batch_idx):
+#         x, y = batch
+#         outputs = self.forward(x)
+#         loss = self.criterion(outputs, y) 
+#         preds = torch.argmax(outputs, dim=1)
+#         acc = accuracy(preds, y)
 
-        self.log('val_loss', loss, prog_bar=True)
-        self.log('val_acc', acc, prog_bar=True)
-        return loss
+#         self.log('val_loss', loss, prog_bar=True)
+#         self.log('val_acc', acc, prog_bar=True)
+#         return loss
 
-    def test_step(self, batch, batch_idx):
-        return self.validation_step(batch, batch_idx)
+#     def test_step(self, batch, batch_idx):
+#         return self.validation_step(batch, batch_idx)
 
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adadelta(self.parameters(), lr=self.learning_rate, )
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=self.gamma)
-        return [optimizer], [scheduler]
-
-
-    def prepare_data(self):
-        torchvision.datasets.MNIST(self.data_dir, train=True, download=True, transform=self.transform)
-        torchvision.datasets.MNIST(self.data_dir, train=False, transform=self.transform)
+#     def configure_optimizers(self):
+#         optimizer = torch.optim.Adadelta(self.parameters(), lr=self.learning_rate, )
+#         scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=self.gamma)
+#         return [optimizer], [scheduler]
 
 
-    def setup(self, stage=None):
-        self.trainset = torchvision.datasets.MNIST(self.data_dir, train=True, download=True, transform=self.transform)
-        self.valset = torchvision.datasets.MNIST(self.data_dir, train=False, transform=self.transform)
+#     def prepare_data(self):
+#         torchvision.datasets.MNIST(self.data_dir, train=True, download=True, transform=self.transform)
+#         torchvision.datasets.MNIST(self.data_dir, train=False, transform=self.transform)
 
-    def train_dataloader(self):
-        return DataLoader(self.trainset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
 
-    def val_dataloader(self):
-        return DataLoader(self.valset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
+#     def setup(self, stage=None):
+#         self.trainset = torchvision.datasets.MNIST(self.data_dir, train=True, download=True, transform=self.transform)
+#         self.valset = torchvision.datasets.MNIST(self.data_dir, train=False, transform=self.transform)
+
+#     def train_dataloader(self):
+#         return DataLoader(self.trainset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
+
+#     def val_dataloader(self):
+#         return DataLoader(self.valset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
 
 
 def main(hparams):
@@ -203,10 +312,17 @@ def main(hparams):
     #     momentum=hparams.momentum, 
     #     batch_size=hparams.batch_size, )
 
-    model = LiMMNIST(arch=hparams.arch,
-        gamma=hparams.gamma,
+    # model = LiMMNIST(arch=hparams.arch,
+    #     gamma=hparams.gamma,
+    #     learning_rate=hparams.lr, 
+    #     batch_size=hparams.batch_size, )
+
+    model = LiMCIFAR100(arch=hparams.arch,
         learning_rate=hparams.lr, 
+        weight_decay=hparams.weight_decay, 
+        momentum=hparams.momentum, 
         batch_size=hparams.batch_size, )
+
 
     logger = TestTubeLogger("tb_logs", name=hparams.expr_name, description=hparams.expr_desc,)
     logger.experiment.tag(vars(hparams)) 
