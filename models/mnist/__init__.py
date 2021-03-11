77247096f71dfa0b0ed24e2fb7f95a3594892210
phuocphn <phuocphn@gmail.com>
2021-03-11 22:50:11 +0900

    add mnist-arch3

--- models/mnist/__init__.py
+++ models/mnist/__init__.py
@@ -1,3 +1,4 @@
 from .arch1 import *
 from .arch2 import *
+from .arch3 import *
 from .LeNet_300_100 import *
---
+++ models/mnist/arch3.py
@@ -0,0 +1,60 @@
+# CNN model definition
+# https://www.kaggle.com/enwei26/mnist-digits-pytorch-cnn-99
+import torch.nn as nn
+import torch.nn.functional as F
+
+## deeper model adapted from https://www.kaggle.com/gustafsilva/cnn-digit-recognizer-pytorch
+class arch3(nn.Module):
+    def __init__(self):
+        super(arch3, self).__init__()
+        
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 32, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 32, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 32, 3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.MaxPool2d(2, 2),
+            nn.Dropout(0.25)
+        )
+        
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, 3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(2, 2),
+            nn.Dropout(0.25)
+        )
+        
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(2, 2),
+            nn.Dropout(0.25)
+        )
+        
+        self.fc = nn.Sequential(
+            nn.Linear(128, 10)
+        )
+                
+        
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        x = F.log_softmax(x, dim=1)
+        return x
